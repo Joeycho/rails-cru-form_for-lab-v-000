ebe2b5518863b2fe56f30e19c5479cb3491561a1
Joey Cho <cjy12182000@naver.com>
2019-02-17 12:56:41 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -13,7 +13,7 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
     @artist.update(artist_params)
     redirect_to artist_path(@artist)
-  end 
+  end
 
 
   private
