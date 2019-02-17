085b2c52adfadcafbda8a84c1bc7c9b71b050f49
Joey Cho <cjy12182000@naver.com>
2019-02-17 12:52:17 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,4 +2,17 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
   end
+
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end
 end
